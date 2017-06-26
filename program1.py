82f0254c4bc411e587e9af7133cb5eca57fe5c5f
sam106lin <sam106.lin@gmail.com>
2017-06-26 17:32:59 -0600

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,10 @@
+print('------文字遊戲------')
+temp=int(input("猜一個數字吧:"))
+if temp == 8:
+    print("你猜對囉!")
+else:
+    print("你猜錯囉!")
+    print("答案是8呀!")
+print("遊戲結束,不玩啦!")
+print("加一行訊息!")
+print("再加一行訊息!")
